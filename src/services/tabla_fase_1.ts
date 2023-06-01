e9c5e00e7c683321023eac29934b7cd712a89872
Sergio-a11 <cz.andrs@gmail.com>
2023-06-01 00:19:44 -0500

    etiquetas tabla fase 1

--- src/services/tabla_fase_1.ts
+++ src/services/tabla_fase_1.ts
@@ -3,18 +3,18 @@ import type { Iteracion } from '../services/obj_ecuacion';
 export function generarTablaFase1(iteraciones: Array<Iteracion>) {
   //encabezado
   let tabla = ""
-  tabla = `<table>
+  tabla = `<table class="tabla-fase-1">
     <thead>
       <tr>
-      <th>CJ</th>`
+      <th class="cj">CJ</th>`
   //CJ
   iteraciones[0].valoresCj.forEach((cj) => {
-    tabla += `<th>${cj}</th>`
+    tabla += `<th class="valores-cj">${cj}</th>`
   })
-  tabla += `<th rowspan="2">xb</th><th rowspan="2">bi</th></tr><tr>`;
-  tabla += `<th>CX</th>`
+  tabla += `<th rowspan="2" class="xb" >xb</th><th rowspan="2">bi</th></tr><tr>`;
+  tabla += `<th class="cx">CX</th>`
   iteraciones[0].variablesArtificialesTexto.forEach((e) => {
-    tabla += `<td>${e}</td>`
+    tabla += `<td class="valores-cj">${e}</td>`
   })
   tabla += `</tr>`
 
@@ -23,19 +23,19 @@ export function generarTablaFase1(iteraciones: Array<Iteracion>) {
     //matriz interna
     //cada fila
     for (let b = 0; b < iteraciones[0].columnaPivote.length; b++) {
-      tabla += `<tr><td>${iteraciones[a].ecuaciones[b].artificial[0]}</td>`;
+      tabla += `<tr><td class="valor-var-artificial" >${iteraciones[a].ecuaciones[b].artificial[0]}</td>`;
       //cada columna
       for (var i = 0; i < iteraciones[0].valoresCj.length; i++) {
-        tabla += `<td>${iteraciones[a].ecuaciones[b].valores[i]}</td>`;
+        tabla += `<td class="celda">${iteraciones[a].ecuaciones[b].valores[i]}</td>`;
       }
-      tabla += `<td>${iteraciones[a].ecuaciones[b].artificial[1]}</td>`;
-      tabla += `<td>${iteraciones[a].ecuaciones[b].resultado}</td></tr>`;
+      tabla += `<td class="letra-var-artificial">${iteraciones[a].ecuaciones[b].artificial[1]}</td>`;
+      tabla += `<td class="valor-bi">${iteraciones[a].ecuaciones[b].resultado}</td></tr>`;
     }
-    tabla += `<tr><td>Zj-Cj</td>`
+    tabla += `<tr><td class="zj-cj">Zj-Cj</td>`
     for (var i = 0; i < iteraciones[0].ZjCj.length; i++) {
-      tabla += `<td>${iteraciones[a].ZjCj[i]}</td>`;
+      tabla += `<td class="valores-zj-cj">${iteraciones[a].ZjCj[i]}</td>`;
     }
-    tabla += `<td>Z</td><td>${iteraciones[a].Z}</td></tr>`
+    tabla += `<td class="z">Z</td><td class="valor-z">${iteraciones[a].Z}</td></tr>`
   }
 
   let tablaFase1 = <HTMLElement>document.getElementById('espacioTablaFase1');
